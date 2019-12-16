a139eb144702e24117de3415ac220e28880cc164
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-12-15 23:10:24 -0300

    Passagem de parametros pelo put extras

--- app/src/main/java/projetos/danilo/jogodaveia/CadastraJogadoresActivity.kt
+++ app/src/main/java/projetos/danilo/jogodaveia/CadastraJogadoresActivity.kt
@@ -1,12 +1,22 @@
 package projetos.danilo.jogodaveia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_cadastra_jogadores.*
 
 class CadastraJogadoresActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastra_jogadores)
+
+        btnJogar.setOnClickListener {
+
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("Jogador1",jogadorUm.text.toString())
+            intent.putExtra("Jogador2",jogadorUm.text.toString())
+            startActivity(intent)
+        }
     }
 }
